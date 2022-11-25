035202f4652a5f7bf29e6fca7f6f4522d74e5123
Md Rahi <fhrfhr102030@gmail.com>
2022-11-25 16:22:09 +0600

    get users data

--- index.js
+++ index.js
@@ -107,6 +107,13 @@ async function run(){
             res.send(users);
         });
 
+        // app.get('/users/admin/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email }
+        //     const user = await usersCollections.findOne(query);
+        //     res.send({ isAdmin: user?.role === 'admin' });
+        // })
+
         app.post('/users', async(req, res) => {
             const user = req.body;
             const result = await usersCollections.insertOne(user);
