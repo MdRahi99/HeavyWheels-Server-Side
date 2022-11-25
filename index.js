6201bf3a32cf7ab6a26ebd24f422bfc4c0aa109f
Md Rahi <fhrfhr102030@gmail.com>
2022-11-25 23:17:13 +0600

    seller api created

--- index.js
+++ index.js
@@ -119,6 +119,18 @@ async function run(){
             const user = await usersCollections.findOne(query);
             res.send({ isAdmin: user?.role === 'admin' });
         })
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollections.findOne(query);
+            res.send({ isBuyer: user?.role === 'buyer' });
+        })
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollections.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        })
     }
     finally{
 
